7ec0299e6389c2738a5e61a09e513c1594388716
reddy <reddy@reddy>
2018-07-03 00:37:12 +0530

    first commit

---
+++ test/src/main/scala/maven/test/wordcount.scala
@@ -0,0 +1,30 @@
+package maven.test
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+
+object wordcount {
+  
+  def main(args: Array[String]) = {
+    
+    val conf = new SparkConf()
+      .setAppName("WordCount")
+      .setMaster("local")
+    val sc = new SparkContext(conf)
+    
+    val test = sc.textFile("food.txt")
+
+    test.flatMap { line =>
+      line.split(" ") 
+    }
+    .map { word =>
+      (word,1) 
+    }
+    .reduceByKey(_ + _) 
+    .saveAsTextFile("food.counts.txt")
+   
+  }
+  
+  
+}
